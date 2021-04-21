1ac5c21a982cd9da8931c9440979983606cc1ce7
Eduardo Omine <eduardo.omine@gmail.com>
2021-04-21 15:22:04 -0300

    Changes link text for meaningful text

--- components/home/hero.js
+++ components/home/hero.js
@@ -6,11 +6,11 @@ export default function Hero({ data }) {
       <h1 className="font-bold mb-4 text-8xl text-gray-50">
         Eduardo Ōmine
       </h1>
-      <p className="mb-6 text-6xl text-gray-100">
+      <p className="leading-tight mb-6 text-6xl text-gray-100">
         Senior front-end developer with <em>15 years of professional experience.</em>
       </p>
       <Link href="/about">
-        <a className="text-xl text-gray-300 tracking-widest uppercase">Read more</a>
+        <a className="text-xl text-gray-300 tracking-widest uppercase">Read full profile</a>
       </Link>
     </section>
   );
